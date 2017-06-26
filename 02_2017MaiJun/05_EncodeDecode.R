cfc35f72c34c9a3ea292fb95d1c8c46541e07b4c
Fpschwartz1 <fpschwartz@gmail.com>
2017-06-25 23:50:53 -0300

    retira dados formatacao

---
+++ 02_2017MaiJun/05_EncodeDecode.R
@@ -0,0 +1,47 @@
+
+################################################
+# Codifica e decodifica RTF
+################################################
+
+library(RCurl)
+library(stringr)
+
+# decodificacao do formato RTF - Base64 para txt
+# https://www.base64decode.org/
+decode_rtf <- function(txt) {
+  txt %>%
+    base64Decode %>%
+    str_replace_all("\\\\'e0", "à") %>%
+    str_replace_all("\\\\'e1", "á") %>%
+    str_replace_all("\\\\'e2", "ã") %>%
+    str_replace_all("\\\\'e3", "ã") %>%
+    str_replace_all("\\\\'e9", "é") %>%
+    str_replace_all("\\\\'e7", "ç") %>%
+    str_replace_all("\\\\'ed", "í") %>%
+    str_replace_all("\\\\'f3", "ó") %>%
+    str_replace_all("\\\\'f5", "õ") %>%
+    str_replace_all("\\\\'f4", "ô") %>%
+    str_replace_all("\\\\'ea", "ê") %>%
+    str_replace_all("\\\\'fa", "ú") %>%
+    str_replace_all("(\\\\[[:alnum:]']+|[\\r\\n]|^\\{|\\}$)", "") %>%
+    str_replace_all("\\{\\{[[:alnum:]; ]+\\}\\}", "") %>%
+    str_trim
+}
+
+encode_rtf <- function(txt) {
+  txt %>%
+    str_replace_all("à", "\\\\'e0") %>%
+    str_replace_all("á", "\\\\'e1") %>%
+    str_replace_all("ã", "\\\\'e2") %>%
+    str_replace_all("ã", "\\\\'e3") %>%
+    str_replace_all("é", "\\\\'e9") %>%
+    str_replace_all("ç", "\\\\'e7") %>%
+    str_replace_all("í", "\\\\'ed") %>%
+    str_replace_all("ó", "\\\\'f3") %>%
+    str_replace_all("õ", "\\\\'f5") %>%
+    str_replace_all("ô", "\\\\'f4") %>%
+    str_replace_all("ê", "\\\\'ea") %>%
+    str_replace_all("ú", "\\\\'fa") %>%
+    str_trim %>%
+    base64Encode 
+}
---
+++ 02_2017MaiJun/06_AjustaDit.R
@@ -0,0 +1,62 @@
+source("05_EncodeDecode.R")
+
+str_retirar <- function(texto){
+  texto <- substr(texto, 1, 100)
+  loc <- str_locate_all(texto, '\\}')[[1]]
+  retirar <- ""
+  if(nrow(loc) > 0){
+    retirar <- substr(texto, 1, loc[nrow(loc), ncol(loc)])
+    loc <- str_locate_all(retirar, '\\{')[[1]]
+    for(l in 1:nrow(loc)){
+      retirar <- paste0(substr(retirar, 1, loc[l, 1]-1), '\\',
+                        substr(retirar, loc[l, 1], str_length(retirar)))
+      loc <- loc + 1
+    }
+    loc <- str_locate_all(retirar, '\\}')[[1]]
+    for(l in 1:nrow(loc)){
+      retirar <- paste0(substr(retirar, 1, loc[l, 1]-1), '\\',
+                        substr(retirar, loc[l, 1], str_length(retirar)))
+      loc <- loc + 1
+    }
+  }
+  retirar
+}
+
+for(ano in 2007:2017){
+  arquivo <- paste0("..\\..\\Dados\\discurso_", ano,"_dit.csv")
+  discursos <- read.csv2(arquivo, sep=";", header = FALSE, colClasses = "character")
+  
+  colnames(discursos) <- c("AnoSeq", "DataHora", "Discurso")
+  
+  # retirar informações de formatação do texto como
+  # exemplo: "\\{\\{ MS Sans Serif;\\}\\{ Symbol;\\}\\{ MS Sans Serif;\\}\\{ Arial;\\}\\}\\{;\\}"
+  
+  for(i in 1:nrow(discursos)){
+    if(!is.na(discursos$Discurso[i])){
+      discurso <- decode_rtf(discursos$Discurso[i])
+      discurso <- gsub("\\*", "", discurso)
+      discurso <- gsub("\\\\", "", discurso)
+      discurso <- gsub('\"', "'", discurso)
+      
+      discurso <- gsub(str_retirar(discurso), "", discurso)
+      
+      discursos$Discurso[i] <- encode_rtf(discurso)
+    }
+  }
+  
+  for(i in 1:nrow(discursos)){
+    if(!is.na(discursos$Discurso[i]) & (discursos$Discurso[i] != "")){
+      discurso <- decode_rtf(discursos$Discurso[i])
+      if(substr(discurso,1,1) == '{'){
+        print(discurso)
+      }
+    }
+  }
+  
+  # grava o último bloco de linhas no arquivo
+  write.table(x=discursos, sep=";",
+              file=arquivo,
+              row.names=FALSE)
+  
+  print(ano)
+}
